e9fd060c1d75ba85599c80a6aeecaa4904d6d312
drumath2237 <drumath2237@users.noreply.github.com>
2021-03-22 09:36:09 +0000

    deploy: b4027474fda97fc4d5e4e3adb9610a3a31ce303d

--- dist/declaration/src/scenes/scripts-map.d.ts
+++ dist/declaration/src/scenes/scripts-map.d.ts
@@ -3,6 +3,7 @@ import { ScriptMap } from "./tools";
  * Defines the interface that exposes all exported scripts in this project.
  */
 export interface ISceneScriptMap extends ScriptMap {
+    "src/scenes/SceneConfig.ts": any;
 }
 /**
  * Defines the map of all available scripts in the project.
